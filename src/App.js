72694d570aba3ae237900d3301cfa6eeec15a616
jeffrey990219 <qs4@rice.edu>
2020-02-18 18:34:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Album from './components/Album';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Album />
+    </div>
+  );
+}
+
+export default App;
