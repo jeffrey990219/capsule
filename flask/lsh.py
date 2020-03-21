8d15d1000ff1e39b7fce5efbb6b1c35e5a93942a
Alan Ji <abj3@rice.edu>
2020-03-21 03:13:58 -0500

    ok fixed

--- flask/lsh.py
+++ flask/lsh.py
@@ -2,8 +2,28 @@ from numpy import *
 from PIL import Image
 import time
 import os
-import functools
-print = functools.partial(print, flush=True)
+import cv2
+from skimage import io
+
+
+def preprocessing(google_photos_urls):
+    for url in google_photos_urls:
+        img = io.imread(url)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        #cv2.imwrite("uploads/google_photos/" + str(i) + ".jpg", img)
+
+        # Extract surf features
+        surf = cv2.xfeatures2d.SURF_create()
+        (kps, descs) = surf.detectAndCompute(img, None)
+        print (len(kps), descs.shape)
+
+        # kps are surf keypoints (https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_surf_intro/py_surf_intro.html)
+        # descs is a 2d array with dimensions (# features , 64) (which is the same as CAPSULE)
+        # probably will need to get only 512 features(rows) out of each image
+        # which ones would we get then??? first 512? what about images that have less features
+
 
 def group(filenames):
-    return []
+    # Return list of lists where each list is an "album"
+    return [["1", "2", "3"], ["4", "5", "6"], ["7", "8"]]
+
--- flask/runserver.py
+++ flask/runserver.py
@@ -1,5 +1,5 @@
 import os, shutil
-from lsh import group
+from lsh import group, preprocessing
 from flask import Flask, render_template, request, session, url_for, redirect
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, patch_request_class, IMAGES
@@ -7,7 +7,6 @@ from werkzeug.utils import secure_filename
 from save_photos import save
 from webbrowser import open
 import functools
-print = functools.partial(print, flush=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,38 +23,40 @@ app.config.update(
 )
 dropzone = Dropzone(app)
 
+# Calls save_photos.py
 @app.route('/open_save_google_photos', methods=['POST', 'GET'])
 def open_save_google_photos():
     if request.method == 'POST':
-        temp = save()
-        print(temp, flush = True)
+        google_photo_urls = save()
+        preprocessing(google_photo_urls)
     return render_template('index.html')
 
-# Saves query images to session['file_urls']
+# Saves query images to session['query_filenames'] (there's no button associated with this)
+# Called by the dropzone stuff
 @app.route('/', methods=['POST', 'GET'])
 def upload():
-    if "file_urls" not in session:
-        session['file_urls'] = []
-    filenames = session['file_urls']
+    if "query_filenames" not in session:
+        session['query_filenames'] = []
+    filenames = session['query_filenames']
     if request.method == 'POST':
-        # Saves query images to session['file_urls']
+        # Saves query images to session['query_filenames']
         for key, f in request.files.items():
-            if key.startswith('file'):
+            if key.startswith('file'):  
                 query_image_path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
                 f.save(query_image_path)
                 filenames.append(query_image_path)
-                print(f)
-        session['file_urls'] = filenames
+        session['query_filenames'] = filenames
     return render_template('index.html')
 
+# Button associated with "Group"
 @app.route('/query', methods=['POST', 'GET'])
 def query():
-    if "file_urls" not in session or session['file_urls'] == []:
+    if "query_filenames" not in session or session['query_filenames'] == []:
         return redirect(url_for('upload'))
-    filenames = session['file_urls']
+    filenames = session['query_filenames']
     # List of lists
     groups = group(filenames)
-    session.pop('file_urls', None)
+    session.pop('query_filenames', None)
     return render_template('results.html', groups = groups)
 
 if __name__ == '__main__':
@@ -64,5 +65,5 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+    open('http://localhost:5555') # opens up two tabs of 5555 for some reason?
     app.run(HOST, PORT, debug = True)
-    save()
