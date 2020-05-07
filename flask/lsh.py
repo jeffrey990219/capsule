120131dd8f3f951e15643887756ffe0b8520a294
Alan Ji <abj3@rice.edu>
2020-05-06 23:46:06 -0500

    cleanup lsh

--- flask/lsh.py
+++ flask/lsh.py
@@ -21,7 +21,7 @@ import imagehash
 
 # 2D matrix is used for each of the L hashtables.
 # Height is 2^K keys
-# Width is number of train images (as bit array)
+# Width is number of images (as bit array)
 class Array2D(object):
     def __init__(self, w, h):
         self.data = array("B", [0] * w * h) # 2d matrix of unsigned bits
@@ -46,17 +46,19 @@ class Array2D(object):
                 s.append(i)
         return s
 
-
+# Used for SimHash. General LSH function.
 def hashfuncFactoryH(m, i, j):
     def f(key):
         return mmh3.hash(key, seed = i * 4 + j) % m;
     return f
 
+# Used for SimHash as the signed hash function.
 def hashfuncFactoryG(m, i, j):
     def f(key):
         return -1 if mmh3.hash(key, seed = (i * 4 + j) + 9999) % 2 == 0 else 1
     return f
 
+# Used for MinHash as the general minhash function given surf feature.
 def MinHash(s, table_index, m):
     hashcodes = []
     for i in range(m):
@@ -95,14 +97,15 @@ def MinHash(s, table_index, m):
 #         for i in range(self.num_features):
 #             surf_feature = des[i:i+1][0]
 #             for j in range(self.L):
+#                 # Create bitarray as hash key
 #                 B = np.zeros(self.K)
 #                 for attr in surf_feature:
 #                     for h in range(self.num_hash_funcs):
 #                         B[self.K // self.num_hash_funcs * h + self.hash_func_lst[j][h](attr)] += attr * self.g_func_lst[j][h](attr)
-#                 b = np.where(B > 0, 1, 0)
+#                 b = np.where(B > 0, 1, 0) # Get sign of bitarray
 #                 b = BitArray(b)
 #                 hash_index = b.uint
-#                 sim_lst = self.bit_array_lst[j].probeBucket(hash_index)
+#                 sim_lst = self.bit_array_lst[j].probeBucket(hash_index) # retrieves bucket of 'similar' images
 #                 for img_index in sim_lst:
 #                     counts_dict[img_index] += 1
 #                     # counts_dict[img_index] += 1.0 / self.num_features_dict[img_index]
@@ -116,7 +119,7 @@ def MinHash(s, table_index, m):
 #         # img2 = cv2.drawKeypoints(key,kp,None,(255,0,0),4)
 #         # cv2.imshow('ok', img2)
 #         # cv2.waitKey(0)
-#         if (len(kp) < self.num_features): ## TODO: need reasonable fix for this
+#         if (len(kp) < self.num_features):
 #             print("Not enough features: ", len(kp))
 #             return None
 
@@ -128,8 +131,7 @@ def MinHash(s, table_index, m):
 #                 for attr in surf_feature:
 #                     for h in range(self.num_hash_funcs):
 #                       B[self.K // self.num_hash_funcs * h + self.hash_func_lst[j][h](attr)] += attr * self.g_func_lst[j][h](attr)
-#                         # 80 / 16 * [0 ... 15] 
-#                 b = np.where(B > 0, 1, 0)
+#                 b = np.where(B > 0, 1, 0) # Get sign of bitarray
 #                 b = BitArray(b)
 #                 hash_index = b.uint
 #                 self.bit_array_lst[j][(bit_index, hash_index)] = True
@@ -145,12 +147,11 @@ class LSH():
         self.num_images = num_images # total number of images
         self.num_hash_funcs = num_hash_funcs
         self.bit_array_lst = [Array2D(self.num_images, 2 ** self.K) for _ in range(self.L)] # hashtables
-        # list of L lists that each contain 16 hash functions
 
     def query(self, key): 
         counts_dict = defaultdict(int)
         (kp, des) = self.surf.detectAndCompute(key, None) # extract features
-        if (len(kp) < self.num_features): ## TODO: need reasonable fix for this
+        if (len(kp) < self.num_features):
             print("Not enough features: ", len(kp))
             return {}
         # For each feature, hash it and probe buckets to find similar images' indices.
@@ -174,7 +175,7 @@ class LSH():
         # img2 = cv2.drawKeypoints(key,kp,None,(255,0,0),4)
         # cv2.imshow('ok', img2)
         # cv2.waitKey(0)
-        if (len(kp) < self.num_features): ## TODO: need reasonable fix for this
+        if (len(kp) < self.num_features):
             print("Not enough features: ", len(kp))
             return {}
 
@@ -193,19 +194,15 @@ class LSH():
             arr.printArray()
             print("-----")
 
-def preprocessing(google_photos_urls):
-    surf = cv2.xfeatures2d.SURF_create(400)
-    #surf.setHessianThreshold(400)
+# def preprocessing(google_photos_urls):
+#     surf = cv2.xfeatures2d.SURF_create(400)
 
-    print("======")
-    for i, url in enumerate(google_photos_urls):
-        img = io.imread(url)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        cv2.imwrite("uploads/google_photos/" + str(i) + ".jpg", img)
-        (kp, des) = surf.detectAndCompute(img, None)
-        
-        #img2 = cv2.drawKeypoints(img1, kp1, None, (255,0,0),4)
-        # plt.imshow(img2), plt.show()
+#     print("======")
+#     for i, url in enumerate(google_photos_urls):
+#         img = io.imread(url)
+#         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#         cv2.imwrite("static/google_photos/" + str(i) + ".jpg", img)
+#         (kp, des) = surf.detectAndCompute(img, None)
 
 # def group(filenames):
 #     # Return list of lists where each list is an "album"
@@ -213,24 +210,17 @@ def preprocessing(google_photos_urls):
 
 def preprocessing2():
     filenames = glob.glob('static/google_photos/*.jpg')
-    print(len(filenames))
-    lsh = LSH(12, 12, 400, len(filenames), 2)
+    # Create LSH object
+    lsh = LSH(14, 12, 400, len(filenames), 2)
+
+    print("-----Inserting ", len(filenames), " images-----")
     for i, filename in enumerate(filenames):
         print(i, filename)
         img = cv2.imread(filename)
         lsh.insert(img, i)
 
-    # num_images = len(glob.glob('uploads/google_photos/*.jpg'))
-    # lsh = LSH(10, 18, 300, num_images, 6) # TODO: find a good K and L
-    # for i, filename in enumerate(glob.glob('uploads/google_photos/*.jpg')):
-    #     print(i, filename)
-    #     filenames.append(filename)
-    #     img = cv2.imread(filename)
-    #     lsh.insert(img, i)
-    #lsh.printArray()
-
     print("-----Querying-----")
-    # Query a single image for any similar images.
+    # Query some images for any similar images.
     queries = [17, 18, 19, 20, 21]
     for i in queries:
         query_image = cv2.imread("static/google_photos/" + str(i) + ".jpg")
@@ -240,40 +230,6 @@ def preprocessing2():
             print(filenames[item[0]], item[1])
         end = time.time()
         print(end - start)
-
-def bruteforce():
-    lsh = LSH(10,12,285,3)
-
-
-    query_image = cv2.imread("uploads/google_photos/17.jpg")
-    des1 = lsh.insert(query_image, 0)
-    # np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # turn off summarization, line-wrapping
-    # with open("17.txt", 'w') as f:
-    #     f.write(np.array2string(des1, separator=', '))
-    # query_image2 = cv2.imread("uploads/google_photos/18.jpg")
-
-    # h1 = imagehash.average_hash(Image.open('uploads/google_photos/17.jpg'))
-    # h2 = imagehash.average_hash(Image.open('uploads/google_photos/18.jpg'))
-    # h3 = imagehash.average_hash(Image.open('uploads/google_photos/14.jpg'))
-
-    # print(h1 - h2)
-    # print(h1 - h3)
-    # des2 = lsh.insert(query_image2, 1)
-    # np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # turn off summarization, line-wrapping
-    # with open("18.txt", 'w') as f:
-    #     f.write(np.array2string(des2, separator=', '))    
-    # query_image3 = cv2.imread("uploads/google_photos/14.jpg")    
-    # des3 = lsh.insert(query_image3, 3)
-    # np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # turn off summarization, line-wrapping
-    # with open("14.txt", 'w') as f:
-    #     f.write(np.array2string(des3, separator=', '))
-    # print(des1.shape)
-    # print(des2.shape)
-    # print(des3.shape)
-
-
-        # des2 = des2.reshape(1,-1)
-
 
 if __name__ == "__main__":
     preprocessing2()
